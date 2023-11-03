329dd126fe2f16ba56b540e9b931c2d6aac1a379
Osman Taha Güneş <ttahaa70@gmail.com>
2023-11-03 21:21:20 +0300

    login added

---
+++ nextjs/src/app/posts/page.tsx
@@ -0,0 +1,10 @@
+import PostsTable from "@/components/posts-table";
+
+export default function Home() {
+    return (
+        <main>
+            <p style={{color:'red'}}>posts page</p>
+            <PostsTable/>
+        </main>
+    );
+}
--- nextjs/src/components/login-form.tsx
+++ nextjs/src/components/login-form.tsx
@@ -1,12 +1,39 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import LockIcon from '@mui/icons-material/Lock';
+import {setUserToken} from '../app/api/login/route'
 export default function FormPropsTextFields() {
-    async function handleLogin () {
-        console.log("login")
+    const [email, setEmail] = React.useState<string | undefined>('Email');
+    const [password, setPassword] = React.useState<string | undefined>('Password');
+
+    const router = useRouter()
+    async function handleLogin() {
+        const data = { email: email, password: password }
+        try {
+            const response = await fetch("http://localhost:3001/auth/login", {
+                method: "POST", // or 'PUT'
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            const result = await response.json();
+            const userToken = result.accessToken;
+            const userId = result.userID;
+            if (!result.error) {
+                router.push('/')
+            }
+            else {
+                console.log("error :", result)
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+
     }
     return (
         <Box
@@ -29,15 +56,16 @@ export default function FormPropsTextFields() {
             height="10vh" // set the height to 100% of the viewport height
 
         >
-            <LockIcon style={{color:'red',fontSize:50}}/>
-            <div style={{ marginTop:10, flexDirection:"column" , borderStyle: 'groove' }}>
-                
+            <LockIcon style={{ color: 'red', fontSize: 50 }} />
+            <div style={{ marginTop: 10, flexDirection: "column", borderStyle: 'groove' }}>
+
                 <TextField
                     required
                     id="filled-required"
                     label="Required"
                     defaultValue="Email"
                     variant="filled"
+                    onChange={(e) => { setEmail(e.target.value); }}
                 />
                 <TextField
                     id="filled-password-input"
@@ -45,12 +73,14 @@ export default function FormPropsTextFields() {
                     type="password"
                     autoComplete="current-password"
                     variant="filled"
+                    onChange={(e) => { setPassword(e.target.value); }}
+
                 />
-                <Button style={{ marginTop:20,marginRight:10}} variant="contained" onClick={handleLogin}>Log in</Button>
+                <Button style={{ marginTop: 20, marginRight: 10 }} variant="contained" onClick={handleLogin}>Log in</Button>
             </div>
             <div style={{ marginTop: 10 }}>
-                <a style={{marginRight:10}}href="/register">Don't have an account? Sign Up</a>
-                <a style={{marginLeft:10}}href="/recovery-password">Forgot password?</a>
+                <a style={{ marginRight: 10 }} href="/register">Don't have an account? Sign Up</a>
+                <a style={{ marginLeft: 10 }} href="/recovery-password">Forgot password?</a>
             </div>
         </Box>
     );
